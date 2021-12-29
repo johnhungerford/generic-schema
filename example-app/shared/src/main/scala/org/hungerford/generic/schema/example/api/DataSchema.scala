c96b99b66362823ab14a13d27757196ed0267614
johnhungerford <hungerfordjustice@gmail.com>
2021-12-28 22:50:11 -0500

    Reformated

--- example-app/shared/src/main/scala/org/hungerford/generic/schema/example/api/DataSchema.scala
+++ example-app/shared/src/main/scala/org/hungerford/generic/schema/example/api/DataSchema.scala
@@ -8,17 +8,17 @@ import org.hungerford.generic.schema.translation.SchemaTranslator
 import sttp.tapir.Schema as TapirSchema
 
 object DataSchema {
-    
+
     val requestSchema = Default.usingDsl( dsl => {
         import dsl.{*, given}
 
         val dateSch = Schema.derived[ Date ]
           .modifyComponent( "year" )(
               _.withValidators( Validator.min( 2000 ), Validator.max( 3000 ) )
-              )
+          )
           .modifyComponent( "month" )(
               _.withValidators( Validator.min( 1 ), Validator.max( 12 ) )
-              )
+          )
         import dateSch.givenSchema
 
         Schema.derived[ Request ]

1ede7a9aeb552ecc9e79a7bcb8f18efcad382197
johnhungerford <hungerfordjustice@gmail.com>
2021-12-30 22:25:07 -0500

    Added basic tapir singleton translation

---
+++ gs-tapir/shared/src/main/scala/org/hungerford/generic/schema/tapir/TapirSchemaSingletonTranslation.scala
@@ -0,0 +1,30 @@
+package org.hungerford.generic.schema.tapir
+
+import org.hungerford.generic.schema.Schema.Aux
+import org.hungerford.generic.schema.coproduct.subtype.TypeName
+import org.hungerford.generic.schema.singleton.SingletonShape
+import org.hungerford.generic.schema.translation.SchemaTranslator
+import sttp.tapir.Schema as TapirSchema
+import sttp.tapir.SchemaType
+import sttp.tapir.Validator as TapirValidator
+
+trait TapirSchemaSingletonTranslation {
+
+    // Default translation of singleton is string with enum validator restring to a single value
+    given [ T <: Singleton, N <: TypeName ] : SchemaTranslator[ T, SingletonShape[ T, N ], TapirSchema ] with {
+        override def translate(
+            schema: Aux[ T, SingletonShape[ T, N ] ]
+        ) : TapirSchema[ T ] = TapirSchema(
+            SchemaType.SString(),
+            schema.name.map( n => TapirSchema.SName( n ) ),
+            false,
+            schema.genericDescription,
+            None,
+            None,
+            schema.genericExamples.headOption,
+            schema.deprecated,
+            TapirValidator.enumeration( List( schema.shape.value ) ),
+        )
+    }
+
+}
--- gs-tapir/shared/src/main/scala/org/hungerford/generic/schema/tapir/TapirSchemaTranslation.scala
+++ gs-tapir/shared/src/main/scala/org/hungerford/generic/schema/tapir/TapirSchemaTranslation.scala
@@ -3,6 +3,7 @@ package org.hungerford.generic.schema.tapir
 trait TapirSchemaTranslation
   extends TapirSchemaProductTranslation
     with TapirSchemaCoproductTranslation
+    with TapirSchemaSingletonTranslation
 
 object TapirSchemaTranslation
   extends TapirSchemaTranslation

56586c909bb10e1a9e176836501bdda3eb01c1a4
johnhungerford <hungerfordjustice@gmail.com>
2022-01-02 21:20:22 -0500

    Added upickle coproduct schema translation

---
+++ gs-upickle/shared/src/main/scala/org/hungerford/generic/schema/upickle/UPickleSchemaTranslation.scala
@@ -0,0 +1,10 @@
+package org.hungerford.generic.schema.upickle
+
+trait UPickleSchemaTranslation
+  extends UPickleProductTranslation
+    with UPickleSingletonSchemaTranslation {
+
+}
+
+object UPickleSchemaTranslation
+  extends UPickleSchemaTranslation

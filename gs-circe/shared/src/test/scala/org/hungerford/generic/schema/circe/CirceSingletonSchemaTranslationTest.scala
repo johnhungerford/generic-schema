779f7b492fa07a0940d3fde78f896de2a60d75ea
johnhungerford <hungerfordjustice@gmail.com>
2022-01-04 10:49:23 -0500

    Separated singleton json translation test from circe singleton schema trans test

--- gs-circe/shared/src/test/scala/org/hungerford/generic/schema/circe/CirceSingletonSchemaTranslationTest.scala
+++ gs-circe/shared/src/test/scala/org/hungerford/generic/schema/circe/CirceSingletonSchemaTranslationTest.scala
@@ -1,53 +1,27 @@
 package org.hungerford.generic.schema.circe
 
+import org.hungerford.generic.schema.translation.{SchemaTranslator, SingletonJsonTranslationTest}
 import org.hungerford.generic.schema.Schema
-import org.hungerford.generic.schema.translation.SchemaTranslator
-import org.scalatest.flatspec.AnyFlatSpecLike
+import io.circe.*
+import CirceSchemaTranslation.given
 
-import io.circe.{Codec, Json}
+class CirceSingletonSchemaTranslationTest
+  extends SingletonJsonTranslationTest[ Codec ] {
+    import SingletonJsonTranslationTest.*
+    import org.hungerford.generic.schema.Default.dsl.*
 
-class CirceSingletonSchemaTranslationTest extends AnyFlatSpecLike with org.scalatest.matchers.should.Matchers {
+    val tsOSchema: Codec[ TestSingleton.type ] = SchemaTranslator.translate( tsSchema )
 
-    behavior of "CirceSingletonSchemaTranslation"
+    def stOSchema: Codec[ SuperT ] = SchemaTranslator.translate( stSchema )
 
-    case object TestSingleton
-
-    it should "translate a singleton to a value that can be encoded and decoded" in {
-        import org.hungerford.generic.schema.Default.dsl.*
-        val sch = Schema.derived[ TestSingleton.type ]
-
-        import org.hungerford.generic.schema.circe.CirceSchemaTranslation.given
-
-        given codec : Codec[ TestSingleton.type ] = SchemaTranslator.translate( sch )
-
-        codec( TestSingleton ).toString shouldBe """"TestSingleton""""
-
-        codec( Json.fromString( "TestSingleton" ).hcursor ) match {
-            case Left( decodeFail ) => fail( decodeFail.message )
-            case Right( v ) => v shouldBe TestSingleton
-        }
+    def writeJson[ T ]( value: T, schm: Codec[ T ] ): String = {
+        given Codec[ T ] = schm
+        schm( value ).noSpaces.toString
     }
 
-    sealed trait SuperT
-    case object SubT extends SuperT
-
-    it should "translate a singleton within a coproduct" in {
-        import org.hungerford.generic.schema.Default.dsl.*
-        val sch = Schema.derived[ SuperT ]
-
-        import org.hungerford.generic.schema.circe.CirceSchemaTranslation.given
-
-        given codec : Codec[ SuperT ] = SchemaTranslator.translate( sch )
-
-        val testVal : SuperT = SubT
-
-        codec( testVal ).toString shouldBe """"SubT""""
-
-        codec( Json.fromString( "SubT" ).hcursor ) match {
-            case Left( decodeFail ) => fail( decodeFail.message )
-            case Right( v ) => v shouldBe SubT
-        }
+    def readJson[ T ]( json: String, schm: Codec[ T ] ): Option[ T ] = {
+        given Codec[ T ] = schm
+        schm( Json.fromString( json ).hcursor ).toOption
     }
-
 
 }
